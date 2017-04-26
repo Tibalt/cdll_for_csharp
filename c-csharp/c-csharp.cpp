b65c953f36bc7b9f17f1270b93e55f307081e4fe
xin zhao <xin zhao>
2017-04-26 14:25:28 +0800

    an example to show how to call c++ gb101 library in c#

--- c-csharp/c-csharp.cpp
+++ c-csharp/c-csharp.cpp
@@ -4,49 +4,95 @@
 #include "stdafx.h"
 //
 #include <stdio.h>
-//
-//extern "C"
-//{
-//	typedef struct {
-//		float value;
-//		char name[128];
-//		int id;
-//	}yc_stru;
-//	yc_stru yc[10]={{1.1,"yc",1},{2.2,"yc",2}};
-//  
-//	
-//	__declspec(dllexport) void DisplayHelloFromDLL()
-//  {
-//    printf ("Hello from DLL !\n");
-//	getchar();
-//  }
-//
-//    __declspec(dllexport) void getYC(int* num, yc_stru *ptr)
-//  {
-//	*num = 2;
-//	memcpy(ptr,yc, sizeof(yc_stru));
-//    printf ("data is copied out from DLL !\n");
-//	getchar();
-//  }
-//
-//}
+#include <stdlib.h>
+
 #define NAG_CALL __stdcall
 #define NAG_DLL_EXPIMP __declspec(dllexport)
 
 extern "C"{
- typedef struct { /* NAG yc Data Type */
- double re,im;
- } yc;
 
-NAG_DLL_EXPIMP void NAG_CALL Tryyc(yc inputVar, yc *outputVar, int n, yc array[]);
-NAG_DLL_EXPIMP void NAG_CALL Tryyc(yc inputVar, yc *outputVar, int n, yc array[])
+
+// pointer of pointer 
+typedef struct{
+     char mem0;
+     char mem1[12];
+     char align[3];
+} mem_struct;
+
+typedef struct { 
+     double re,im;
+     char arr[8];
+     mem_struct memstru1;
+     double th;
+} yc;
+
+typedef struct { 
+     char arr[40];
+} yc_wrapper;
+
+
+
+//NAG_DLL_EXPIMP void NAG_CALL Tryyc(yc inputVar, yc *outputVar, int n, yc array[]);
+
+ void Tryyc(yc inputVar, yc **outputVar, int n, yc array[])
 {
- outputVar->re = ++inputVar.re;
- outputVar->im = ++inputVar.im;
- array[0].re = 99.0;
- array[0].im = 98.0;
- array[1].re = 97.0;
- array[1].im = 96.0;
+     *outputVar = (yc*)malloc(sizeof (yc));
+     (*outputVar)->re = ++inputVar.re;
+     (*outputVar)->im = ++inputVar.im;
+     (*outputVar)->arr[0] = inputVar.arr[0] + 10;
+     (*outputVar)->th = ++inputVar.th;
 
+     array[0].re = 99.0;
+     array[0].im = 98.0;
+     array[1].re = 97.0;
+     array[1].im = 96.0;
 } 
+NAG_DLL_EXPIMP void NAG_CALL TryYCwrapper(yc inputVar, yc_wrapper **outputVar)//, yc_wrapper array[]){
+{
+     //yc iyc;
+     //iyc.re = 1.0;
+     //iyc.im =1.1;
+     yc arr[10];
+     Tryyc(inputVar,(yc**) outputVar, 10,  arr);
+}
+NAG_DLL_EXPIMP void NAG_CALL printYCwrapper( yc_wrapper *outputVar){
+     yc* ptr = (yc*)(outputVar);
+     printf("re=%f,im=%f\n",ptr->re,ptr->im);
+     printf("arr[0]=%d\n",ptr->arr[0]);
+     printf("th=%f\n",ptr->th);
+}
+//Point
+typedef struct
+{
+
+     unsigned int addr;
+
+     float Yc_Value;
+     unsigned char OV ;
+     unsigned char BL ;
+     unsigned char SB ;
+     unsigned char NT ;
+     unsigned char IV ;
+
+}Point;
+
+typedef struct
+{
+     unsigned int Yc_Num;
+     Point Yc_Data[128];
+}PointsStru;
+
+NAG_DLL_EXPIMP void NAG_CALL PointAssign(PointsStru *outputVar )
+{
+     outputVar->Yc_Num = 128;
+     outputVar->Yc_Data[2].Yc_Value = 123.45;
+     outputVar->Yc_Data[2].BL = 1;
+     outputVar->Yc_Data[2].SB = 2;
+
+
+     outputVar->Yc_Data[127].Yc_Value = 567.89;
+     outputVar->Yc_Data[127].BL = 2;
+     outputVar->Yc_Data[127].SB = 3;
+
+}
 }
---
+++ call-c/Program.cs
@@ -0,0 +1,109 @@
+﻿/***************************************************************
+ * C# Class *
+ ***************************************************************/
+using System;
+using System.Runtime.InteropServices;
+
+
+namespace DDJexamples
+{
+    // Nag yc type
+    [StructLayout(LayoutKind.Sequential)]
+    unsafe public struct mem_struct
+    {
+        public byte mem0;
+        public fixed byte mem1[12];
+        public fixed byte align[3];
+    }
+    unsafe public struct yc
+    {
+        public double re;
+        public double im;
+        public fixed byte arr[8];
+        public mem_struct mem;
+        public double th;
+        //public fixed byte arr[16];
+    };
+    unsafe public struct yc_wrapper
+    {
+        public fixed byte arr[36];
+    };
+
+    unsafe public struct Point
+    {
+        public uint addr;
+        public float Yc_Value;
+        public byte OV;
+        public byte BL;
+        public byte SB;
+        public byte NT;
+        public byte IV;
+
+    }
+
+    unsafe public struct Data_Stru
+    {
+        public uint Yc_Num;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 128)]
+        public Point[] Yc_Data;
+    }
+
+
+    public class ExerciseTryyc
+    {
+
+        [DllImport("c-csharp.dll")]
+        unsafe public static extern void TryYCwrapper(yc inputVar, yc_wrapper** outputVar);
+        [DllImport("c-csharp.dll")]
+        unsafe public static extern void printYCwrapper(yc_wrapper* outputVar);
+        [DllImport("c-csharp.dll")]
+        unsafe public static extern void PointAssign(ref Data_Stru outputVar);
+
+  
+        unsafe public static void Main()
+        {
+            int n = 2;
+            yc inputVar = new yc();
+            yc outputVar = new yc();
+            yc[] array = new yc[10];
+
+            Data_Stru y_data = new Data_Stru();
+
+            yc_wrapper* wrapperVar = null;// = new yc_wrapper();
+
+            inputVar.re = 1.0;
+            inputVar.im = 2.0;
+            inputVar.arr[0] = 100;
+            inputVar.th = 3.0;
+
+            TryYCwrapper(inputVar, &wrapperVar);
+            Console.WriteLine("Print out in dll with the yc value handled by the same dll just now!");
+            Console.WriteLine("See function TryYCwrapper in dll for the value supposed to be.");
+            Console.WriteLine("-----------------------------------------------------------------------");
+            printYCwrapper(wrapperVar);
+
+
+            Console.WriteLine("");
+            Console.WriteLine("print out values got from dll in C# !");
+            PointAssign(ref y_data);
+            Console.WriteLine("-----------------------------------------------------------------------");
+            Console.WriteLine("data = ({0},{1},{2},{3})", y_data.Yc_Num, y_data.Yc_Data[127].Yc_Value, y_data.Yc_Data[127].BL, y_data.Yc_Data[127].SB);
+            Console.WriteLine("data = ({0},{1},{2},{3})", y_data.Yc_Num, y_data.Yc_Data[2].Yc_Value, y_data.Yc_Data[2].BL, y_data.Yc_Data[2].SB);
+            Console.ReadLine();
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
